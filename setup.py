1d9d6b40dd103f79f5b5f95c88f5e37df6002834
vitostamatti <vitostamatti@gmail.com>
2022-09-05 15:04:26 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+
+setup(
+    name='importance_feature_selector',
+    version='0.1.0',
+    description='feature selection based on feature importance',
+    # long_description=long_description,
+    license="MIT",
+    author='Vito Stamatti',
+    package_dir={'':'.'},
+    packages=find_packages(where='.'),
+    install_requires=[
+        'scikit-learn', 
+    ],
+),
